a72e5a35f98102f8055a6e73e7c4be8a617dc125
Logan Dhillon <dev@logandhillon.com>
2026-02-27 00:50:23 -0500

    feat: website

---
+++ site/app.js
@@ -0,0 +1,5 @@
+function onLoad() {
+  document.getElementById("copyright").textContent = `© 2024-${new Date().getFullYear()} logandhillon.com • Created by/for 5409 Chargers.`;
+}
+
+document.addEventListener("DOMContentLoaded", onLoad);
